8f25a2d2ad224618407deb0c0eddf90b9a0bf7ac
vishwajeet kumar <157208806+vishwajeet059161@users.noreply.github.com>
2024-02-25 14:11:06 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,30 @@
+let input = document.getElementById('inputbox');
+let buttons = document.querySelectorAll
+('button');
+
+ let String ="";
+ let arr = Array.from(buttons);
+ arr.forEach(button => {
+    button.addEventListener('click' ,(e) =>{
+        if(e.target.innerHTML == '='){
+            String = eval(String);
+            input.value = String;
+        }
+        else if(e.target.innerHTML == 'AC'){
+            String = "";
+            input.value = String;
+        }
+        else if(e.target.innerHTML =='DEL'){
+            String = String.substring(0,String.length-1);
+            input.value = String;
+        }
+        else{
+
+        
+            String += e.target.innerHTML;
+            input.value = String; 
+        } 
+          
+    } )
+ })
+
